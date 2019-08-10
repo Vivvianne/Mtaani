6b6fd8e313e7709856eea8599d4dff2c6be4f8f8
Vivvianne Kimani <ndiahav@gmail.com>
2019-08-10 17:46:34 +0300

    added classes in form.py for userupdate,and profile pdate

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    identity_number = models.IntegerField()
+    # neighbour_number = models.IntegerField()
     email = models.CharField(max_length=30)
     
     def __str__(self):
