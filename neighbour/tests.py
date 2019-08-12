2f2bb4804c942e26207cf855ff101dae73ce6462
Vivvianne Kimani <ndiahav@gmail.com>
2019-08-12 15:09:11 +0300

    added tests

--- neighbour/tests.py
+++ neighbour/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Business, Neighbourhood, Post
 
-# Create your tests here.
+
+def setUp(self):
+    self.user1= User(id=1,username='Tester',email='tester@gmail.com',password='testing321')
+    self.user1.save()
+    self.neighbourhood = Neighbourhood(id=1,name='sar',location='kibera')
+    self.neibourhood.create_neigborhood()
+    self.post = Post(title= 1,content='sar',author='tester')
+    self.neibourhood.create_neigborhood()
+
+
+def tearDown(self):
+    Post.objects.all().delete()
+    User.objects.all().delete()
+    Neighbourhood.objects.all().delete()
+    
+    
+def test_is_instance(self):
+    self.assertTrue(isinstance(self.user1,User))
+    self.assertTrue(isinstance(self.post,Post))
+    self.assertTrue(isinstance(self.neiba,Neighbourhood))
+    
+    
+def test_save_method(self):
+    self.post.create_post()
+    all_objects = Post.objects.all()
+    self.assertTrue(len(all_objects)>0)
+    
+    
+def test_save_method(self):
+    self.neibourhood.create_neigborhood()
+    all_objects = Neighbourhood.objects.all()
+    self.assertTrue(len(all_objects)>0)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.test_profile = Profile(title=list('Beautiful butterflies'))
+        self.test_profile.save_profile()
+
+       
+
+    def tearDown(self):
+        Profile.objects.all().delete()
